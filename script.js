b206be197d21b5d00354a900888460a37135e5b8
Md Hasib <md.hasibuzzaman001@google.com>
2022-08-19 19:10:11 +0600

    some bug fix

--- script.js
+++ script.js
@@ -1,102 +1,146 @@
-const addCounterEl = document.getElementById("add_counter");
-const countersEl = document.getElementById("counters");
-const resetEl = document.getElementById("reset");
+// dom element target
+const addCounterBtn = document.getElementById("addCounterBtn");
+const resetBtn = document.getElementById("resetBtn");
+const parentCounter = document.getElementById("parentCounter");
 
-const initialState = [{ id: 1, value: 0 }];
-// action identifiers
-const ADD_COUNTER = "addCounter";
-const RESET = "reset";
+const incrementBtn = document.getElementById("incrementBtn");
+const decrementBtn = document.getElementById("decrementBtn");
+
+// const variable
 const INCREMENT = "increment";
 const DECREMENT = "decrement";
+const ADD_STORE = "addStore";
+const RESET = "reset";
 
-//Action creator
-const increment = (id) => {
+// action creator
+const incrementAction = (id) => {
   return {
     type: INCREMENT,
     payload: id,
   };
 };
-
-const decrement = (id) => {
+const decrementAction = (id) => {
   return {
     type: DECREMENT,
     payload: id,
   };
 };
-function counterReducer(state = initialState, action) {
-  const rest = state.filter((st) => {
-    return st.id != action.payload;
-  });
-  const modify = state.find((st) => {
-    return st.id === action.payload;
-  });
+const addStoreAction = () => {
+  return {
+    type: ADD_STORE,
+  };
+};
+const resetAction = () => {
+  return {
+    type: RESET,
+  };
+};
 
-  console.log("rest", rest);
-  console.log("mod", modify);
-  if (action.type === ADD_COUNTER) {
-    return [
-      ...state,
-      {
-        id: state.length + 1,
-        value: 0,
-      },
-    ];
+// initial state
+let initialState = [{ id: 1, value: 0 }];
+
+// random number generator
+const randomNumber = () => {
+  return Math.floor(Math.random() * 10 + 1);
+};
+
+// reducer function
+const reducerCounter = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      const incrementCounter = state.map((c) => {
+        if (c.id === action.payload) {
+          return {
+            ...c,
+            value: c.value + c.id,
+          };
+        } else {
+          return { ...c };
+        }
+      });
+      return incrementCounter;
+    case DECREMENT:
+      const decrementCounter = state.map((c) => {
+        if (c.id === action.payload) {
+          return {
+            ...c,
+            value: c.value - c.id,
+          };
+        } else {
+          return { ...c };
+        }
+      });
+      return decrementCounter;
+    case ADD_STORE:
+      return [...state, { id: state.length + 1, value: 0 }];
+    case RESET:
+      const resetValue = state.map((val) => {
+        return {
+          ...val,
+          value: 0,
+        };
+      });
+      return resetValue;
+
+    default:
+      return state;
   }
-  if (action.type === RESET) {
-    const resetState = [...state]
-    for(let i = 0; i<state.length; i++){
-        resetState[i].value = 0;
-    }
-    return [...resetState];
-  } else if (action.type === INCREMENT) {
-    const modifiedObj = {
-      ...modify,
-      value: modify.value + action.payload,
-    };
-    return [...rest, modifiedObj];
-  } else if (action.type === DECREMENT) {
-    const modifiedObj = {
-      ...modify,
-      value: modify.value - action.payload,
-    };
-    return [...rest, modifiedObj];
-  } else {
-    return state;
-  }
-}
+};
 
 // create store
-const store = Redux.createStore(counterReducer);
+const store = Redux.createStore(reducerCounter);
 
-const render = () => {
-  const state = store.getState();
-  countersEl.innerHTML = state.map((counter) => {
-    return `<div
-  class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow mb-8"
-  >
-  <div class="text-2xl font-semibold">${counter.value}</div>
-  <div class="flex space-x-3">
-    <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" onclick=store.dispatch(increment(${counter.id}))>
-      Increment
-    </button>
-    <button class="bg-red-400 text-white px-3 py-2 rounded shadow" onclick=store.dispatch(decrement(${counter.id}))>
-      Decrement
-    </button>
-  </div>
-</div>`;
-  });
+const render = (id) => {
+  if (id) {
+    const updatedData = store.getState();
+    const changeValue = updatedData.filter((val) => val.id === id);
+    const targetUi = document.getElementById(id);
+    targetUi.innerText = changeValue[0].value;
+  } else {
+    const updatedData = store.getState();
+    updatedData.map((val) => {
+      const id = val.id;
+      document.getElementById(id).innerText = val.value;
+    });
+  }
 };
-//render ui initially
-render();
-store.subscribe(render);
 
-addCounterEl.addEventListener("click", () => {
-  store.dispatch({
-    type: ADD_COUNTER,
-  });
+store.subscribe(() => render());
+// children create function
+const children = () => {
+  const parentChildren = document.getElementById("parentCounter").children;
+  const id = parentChildren.length + 1;
+  const div = document.createElement("div");
+  div.innerHTML = `
+        <div
+          class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+        >
+          <div class="text-2xl font-semibold counter" id=${id}>0</div>
+          <div class="flex space-x-3">
+            <button
+              class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+              onclick="store.dispatch(incrementAction(${id}))"
+            >
+              Increment
+            </button>
+            <button
+              class="bg-red-400 text-white px-3 py-2 rounded shadow"
+              onclick="store.dispatch(decrementAction(${id}))"
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
+  `;
+  parentCounter.appendChild(div);
+};
+// first time children create
+children();
+// event listener
+resetBtn.addEventListener("click", () => {
+  store.dispatch(resetAction());
 });
-resetEl.addEventListener("click", () => {
-  store.dispatch({
-    type: RESET,
-  });
+addCounterBtn.addEventListener("click", () => {
+  children();
+  store.dispatch(addStoreAction());
 });
